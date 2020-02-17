dd7cc5edae5fb5798b4089234dc0c019da886448
Valerie Lui <vallq@users.noreply.github.com>
2020-02-17 17:07:16 +0800

    Added POST methods for authentication

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const User = require("./models/user.model");
+require("dotenv").config();
+const userRouter = require("./routes/user.route");
+
+const corsOptions = {
+  credentials: true,
+  allowedHeaders: "content-type",
+  origin: "http://localhost:3001"
+};
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
 
 app.use(express.json());
+app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
   const apiEndpoints = {
@@ -15,6 +32,56 @@ app.get("/", (req, res) => {
     "7": "POST /user/pastworkouts"
   };
   res.send(apiEndpoints);
+});
+
+const createJWT = username => {
+  const payload = { name: username };
+  //console.log("secret key is" + process.env.JWT_SECRET_KEY);
+  //console.log("payload is", payload);
+  const token = jwt.sign(payload, process.env.JWT_SECRET_KEY);
+  //console.log("Token is", token);
+  return token;
+};
+
+app.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    //res.send("hi");
+    ///const user = await User.findOne({ username });
+    const user = {
+      username,
+      password
+    };
+    //const result = await bcrypt.compare(password, user.password);
+
+    console.log("hi");
+
+    // if (!result) {
+    //   throw new Error("Login Failed");
+    // }
+
+    const token = createJWT(user.username);
+    console.log("token 2 is", token);
+
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneWeek = oneDay * 7;
+    const expiryDate = new Date(Date.now() + oneWeek);
+
+    res.cookie("token", token, {
+      expires: expiryDate
+    });
+
+    res.send("Welcome!");
+  } catch (err) {
+    if (err.message === "Login Failed") {
+      err.statusCode = 400;
+    }
+    next(err);
+  }
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("token").send("You are now logged out!");
 });
 
 app.use((err, req, res, next) => {
