13e6d0d0cd7daf305b36813290a0e3d1d1917eb5
Valerie Lui <vallq@users.noreply.github.com>
2020-02-17 17:03:38 +0800

    Modified index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const PORT = 3000;
 const app = require("./app");
 
+
 const server = app.listen(process.env.PORT || PORT, () => {
   console.log(`Express app started on http://localhost:${PORT}`);
 });
