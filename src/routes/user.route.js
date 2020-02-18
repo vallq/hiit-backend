aac169d2e61bec73d10cd75365b0caeafe1886c7
Valerie Lui <vallq@users.noreply.github.com>
2020-02-18 13:44:28 +0800

    Added routes for GET workout by ID and DELETE workout by ID when authorised

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user.model");
+const { protectRoute } = require("../middleware/auth");
 const WORKOUTS_FIELD = "workouts";
 
 router.get("/", async (req, res) => {
@@ -8,7 +9,7 @@ router.get("/", async (req, res) => {
     {},
     `-__v -_id -password -${WORKOUTS_FIELD}._id -${WORKOUTS_FIELD}.isCompleted`
   );
-  users.sort((a, b) => a.username > b.username);
+  users.sort((a, b) => b.username > a.username);
   res.status(200).send(users);
 });
 
@@ -30,7 +31,7 @@ router.get("/:username/pastworkouts", async (req, res) => {
   // const expectedUserWorkout = {
   //   workouts: [
   //     {
-    //      id: 1
+  //      id: 1,
   //       duration: 15,
   //       focus: "upper body",
   //       exercises: ["pushups", "planks", "superman", "pull ups"]
@@ -44,4 +45,39 @@ router.get("/:username/pastworkouts", async (req, res) => {
   res.status(200).send(expectedUserWorkout);
 });
 
+router.get("/:username/pastworkouts/:id", async (req, res, next) => {
+  try {
+    const targetUsername = req.params.username;
+    const targetId = req.params.id;
+    // const expectedUserWorkout = {
+    //        id: 1,
+    //       duration: 15,
+    //       focus: "upper body",
+    //       exercises: ["pushups", "planks", "superman", "pull ups"]
+    // };
+
+    const userWorkouts = await User.findOne(
+      { username: targetUsername, "workouts.id": targetId },
+      `-__v -_id -username -password -${WORKOUTS_FIELD}._id -${WORKOUTS_FIELD}.isCompleted`
+    );
+    if (userWorkouts === null) {
+      throw new Error("Workout not found");
+    }
+    const expectedUserWorkout = userWorkouts.workouts[0];
+    res.status(200).send(expectedUserWorkout);
+  } catch (err) {
+    if (err.message === "Workout not found") {
+      err.statusCode = 400;
+    }
+    next(err);
+  }
+});
+
+router.delete("/:username/pastworkouts/:id", protectRoute, async (req, res) => {
+  const targetUsername = req.params.username;
+  const targetId = req.params.id;
+  const targetWorkout = await User.findOneAndDelete({ username: targetUsername, "workouts.id": targetId },
+  `-__v -_id -username -password -${WORKOUTS_FIELD}._id -${WORKOUTS_FIELD}.isCompleted`);
+  res.status(200).send(targetWorkout);
+})
 module.exports = router;
