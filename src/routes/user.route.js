0585944ee334952e7ce1165d25f73b25508406e2
Valerie Lui <vallq@users.noreply.github.com>
2020-02-19 11:00:11 +0800

    Added PATCH route to update workouts

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,14 +2,18 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.model");
 const { protectRoute } = require("../middleware/auth");
+const { removeId } = require("../utils/helperFunctions");
 const WORKOUTS_FIELD = "workouts";
+const returnWorkoutsOnly = `-__v -_id -username -password -${WORKOUTS_FIELD}._id -${WORKOUTS_FIELD}.isCompleted`;
+const returnUsersOnly = `-__v -_id -password -${WORKOUTS_FIELD}._id -${WORKOUTS_FIELD}.isCompleted`;
+const returnTargetUserAndWorkouts =  "-__v -_id -password";
 
 router.get("/", async (req, res) => {
   const users = await User.find(
     {},
-    `-__v -_id -password -${WORKOUTS_FIELD}._id -${WORKOUTS_FIELD}.isCompleted`
+    returnUsersOnly
   );
-  users.sort((a, b) => b.username > a.username);
+  users.sort();
   res.status(200).send(users);
 });
 
@@ -21,7 +25,7 @@ router.get("/:username", async (req, res) => {
   // };
   const expectedUser = await User.findOne(
     { username: targetUsername },
-    "-__v -_id -password"
+   returnTargetUserAndWorkouts
   );
   res.status(200).send(expectedUser);
 });
@@ -40,7 +44,7 @@ router.get("/:username/pastworkouts", async (req, res) => {
   // };
   const expectedUserWorkout = await User.findOne(
     { username: targetUsername },
-    `-__v -_id -username -password -${WORKOUTS_FIELD}._id -${WORKOUTS_FIELD}.isCompleted`
+    returnWorkoutsOnly
   );
   res.status(200).send(expectedUserWorkout);
 });
@@ -58,7 +62,7 @@ router.get("/:username/pastworkouts/:id", async (req, res, next) => {
 
     const userWorkouts = await User.findOne(
       { username: targetUsername, "workouts.id": targetId },
-      `-__v -_id -username -password -${WORKOUTS_FIELD}._id -${WORKOUTS_FIELD}.isCompleted`
+      returnWorkoutsOnly
     );
     if (userWorkouts === null) {
       throw new Error("Workout not found");
@@ -73,11 +77,73 @@ router.get("/:username/pastworkouts/:id", async (req, res, next) => {
   }
 });
 
-router.delete("/:username/pastworkouts/:id", protectRoute, async (req, res) => {
-  const targetUsername = req.params.username;
-  const targetId = req.params.id;
-  const targetWorkout = await User.findOneAndDelete({ username: targetUsername, "workouts.id": targetId },
-  `-__v -_id -username -password -${WORKOUTS_FIELD}._id -${WORKOUTS_FIELD}.isCompleted`);
-  res.status(200).send(targetWorkout);
-})
+router.delete(
+  "/:username/pastworkouts/:id",
+  protectRoute,
+  async (req, res, next) => {
+    const INCORRECT_ERR = "Incorrect user";
+    try {
+      const targetUsername = req.params.username;
+      const targetId = req.params.id;
+      if (req.user.name !== targetUsername) {
+        throw new Error(INCORRECT_ERR);
+      }
+      const targetWorkout = await User.findOneAndDelete(
+        { username: targetUsername, "workouts.id": targetId },
+        {
+          projection: {
+            __v: 0,
+            _id: 0,
+            username: 0,
+            password: 0,
+            "workouts._id": 0,
+            "workouts.isCompleted": 0
+          }
+        }
+      );
+      workout = targetWorkout.workouts[0];
+      res.status(200).send(workout);
+    } catch (err) {
+      err.messsage = INCORRECT_ERR;
+      err.statusCode = 403;
+      next(err);
+    }
+  }
+);
+
+router.patch(
+  "/:username/pastworkouts",
+  protectRoute,
+  async (req, res, next) => {
+    const INCORRECT_ERR = "Incorrect user";
+    try {
+      const targetUsername = req.params.username;
+      if (req.user.name !== targetUsername) {
+        throw new Error(INCORRECT_ERR);
+      }
+      const workoutToAdd = {
+        id: 2,
+        duration: 15,
+        focus: "core",
+        exercises: [
+          "sit ups",
+          "reverse crunches",
+          "flutter kicks",
+          "russian twists"
+        ]
+      };
+      const { workouts } = await User.findOne(
+        { username: targetUsername },
+        returnWorkoutsOnly,
+      );
+      workouts.push(workoutToAdd);
+      const updatedWorkouts = workouts.toObject().map(removeId);
+      res.send(updatedWorkouts);
+    } catch (err) {
+      err.message = "Unable to add new workout";
+      next(err);
+    }
+  }
+);
+
 module.exports = router;
