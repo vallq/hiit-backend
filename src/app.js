eba54c0a50d39989d767d6947de9eb2c42663870
Valerie Lui <vallq@users.noreply.github.com>
2020-02-26 18:08:33 +0800

    Removed login and logout routes

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
@@ -5,17 +6,17 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const User = require("./models/user.model");
-require("dotenv").config();
 const userRouter = require("./routes/user.route");
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://hiit-generator.netlify.com"
-];
+const { wrapAsync } = require("./utils/helperFunctions");
 
 const corsOptions = {
   credentials: true,
   allowedHeaders: "content-type",
-  origin: "http://localhost:3000"
+  origin: [
+    process.env.FRONTEND_URI,
+    "http://localhost:3000",
+    "http://localhost:3001"
+  ]
 };
 
 app.use(cors(corsOptions));
@@ -24,63 +25,23 @@ app.use(cookieParser());
 app.use(express.json());
 app.use("/user", userRouter);
 
-app.get("/", (req, res) => {
-  const apiEndpoints = {
-    "0": "GET / all API endpoints",
-    "1": "POST /register new user",
-    "2": "POST /login",
-    "3": "POST /logout",
-    "4": "GET /user returns user details",
-    "5": "GET /user/pastworkouts",
-    "6": "GET /user/pastworkouts/:id",
-    "7": "DELETE /user/pastworkouts/:id",
-    "8": "POST /user/pastworkouts"
-  };
-  res.send(apiEndpoints);
-});
-
-const createJWT = username => {
-  const payload = { name: username };
-  const token = jwt.sign(payload, process.env.JWT_SECRET_KEY);
-  return token;
-};
-
-app.post("/login", async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
-
-    const user = await User.findOne({ username });
-    const result = await bcrypt.compare(password, user.password);
-
-    if (!result) {
-      throw new Error("Login Failed");
-    }
-
-    const token = createJWT(user.username);
-
-    const oneDay = 24 * 60 * 60 * 1000;
-    const oneWeek = oneDay * 7;
-    const expiryDate = new Date(Date.now() + oneWeek);
-
-    res.cookie("token", token, {
-      expires: expiryDate
-    });
-
-    res.send("Welcome!");
-  } catch (err) {
-    if (
-      err.message === "Login Failed" ||
-      err.message === "No login credentials given"
-    ) {
-      err.statusCode = 400;
-    }
-    next(err);
-  }
-});
-
-app.post("/logout", (req, res) => {
-  res.clearCookie("token").send("You are now logged out!");
-});
+app.get(
+  "/",
+  wrapAsync(async (req, res) => {
+    const apiEndpoints = {
+      "0": "GET / all API endpoints",
+      "1": "GET /user returns user details",
+      "2": "POST /user/register new user",
+      "3": "POST /user/login",
+      "4": "POST /user/logout",
+      "5": "GET /user/pastworkouts",
+      "6": "GET /user/pastworkouts/:id",
+      "7": "POST /user/pastworkouts",
+      "8": "DELETE /user/pastworkouts/:id"
+    };
+    await res.send(apiEndpoints);
+  })
+);
 
 app.use((err, req, res, next) => {
   res.status(err.statusCode || 500);
