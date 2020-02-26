4c59a0764d7f06f1ebcc45d51dbdb2c186e8cc3e
Valerie Lui <vallq@users.noreply.github.com>
2020-02-26 18:06:51 +0800

    Moved login and logout tests

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -64,51 +64,19 @@ describe("app", () => {
   it("GET / should return all API endpoints", async () => {
     const apiEndpoints = {
       "0": "GET / all API endpoints",
-      "1": "POST /register new user",
-      "2": "POST /login",
-      "3": "POST /logout",
-      "4": "GET /user returns user details",
+      "1": "GET /user returns user details",
+      "2": "POST /user/register new user",
+      "3": "POST /user/login",
+      "4": "POST /user/logout",
       "5": "GET /user/pastworkouts",
       "6": "GET /user/pastworkouts/:id",
-      "7": "DELETE /user/pastworkouts/:id",
-      "8": "POST /user/pastworkouts"
+      "7": "POST /user/pastworkouts",
+      "8": "DELETE /user/pastworkouts/:id"
     };
     const { body: response } = await request(app)
       .get("/")
       .expect(200)
       .send(apiEndpoints);
     expect(response).toEqual(apiEndpoints);
-  });
-  describe("/login", () => {
-    it("POST / should return `Welcome!` when user is authenticated and valid", async () => {
-      const expectedUser = {
-        username: "warrior123",
-        password: "iWannaB3DVeryBest"
-      };
-      const { text: correctMessage } = await request(app)
-        .post("/login")
-        .send(expectedUser)
-        .expect(200);
-      expect(correctMessage).toEqual("Welcome!");
-    });
-
-    it("POST / should return `Login Failed` when password is incorrect", async () => {
-      const wrongPassword = {
-        username: "warrior123",
-        password: "iWannaB3DBest"
-      };
-      const { text: errorMessage } = await request(app)
-        .post("/login")
-        .send(wrongPassword)
-        .expect(400);
-      expect(errorMessage).toBe(`{"error":"Login Failed"}`);
-    });
-
-    it("POST / should return `You are now logged out!` when user goes to /logout", async () => {
-      const { text: message } = await request(app)
-        .post("/logout")
-        .expect(200);
-      expect(message).toBe("You are now logged out!");
-    });
   });
 });
